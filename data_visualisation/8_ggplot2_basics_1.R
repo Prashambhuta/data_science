5c845fe8cb256ba1b48cc31d101ab2dda6bb4ab1
Prashambhuta <prasham.bhuta@gmail.com>
2020-04-08 11:03:57 +0530

    Data Visualisation Basics

---
+++ data_visualisation/8_ggplot2_basics_1.R
@@ -0,0 +1,36 @@
+# murders dataset is already loaded from in the environment
+#
+# first import ggplot library
+library(tidyverse)
+
+# define a ggplot object and associate data with it
+p <- murders %>% ggplot(aes(population_in_millions, total, label=abb)) +
+
+# you can also use pipe %>%
+# eg `murders %>% ggplot()`
+
+# adding geom_label & geom_text to the plots
+geom_text(nudge_x = 0.04, nudge_y = 0.031) +
+
+# changing scales to logarithmic
+scale_x_continuous(trans = "log10") +
+scale_y_continuous(trans = "log10") + 
+xlab("Population in Millions") +
+ylab("Total (Log scale)") +
+    # adding title
+    ggtitle("US total murders for state vs state population")
+    
+# define the line of avg murder rate in us
+r <- murders %>% summarise(rate = sum(total) / (sum(population)) * 10^6) %>% .$rate
+    
+        
+    # adding abline of average murders rate
+    p + geom_abline(intercept = log10(r), colour = "darkgrey", linetype = 2, size = 1.3 ) +
+    
+    # adding geom_point with color argument
+    geom_point(aes(col = region), size = 2.4) +
+        
+    # capitalising the label `region`
+    scale_color_discrete(name = "Regions in US")
+
+
--- data_visualisation/box_plots_basics.R
+++ data_visualisation/box_plots_basics.R
@@ -6,3 +6,8 @@ library(dslabs)
 data(murders)
 
 rate_per_million <- (murders$total/(murders$population/1000000))
+boxplot(rate_per_million, xlab="US states", ylab="Rate per Million Population")
+
+# Comparing male & female heights. Boxplot is useful for such cases.
+females <- heights$height[heights$sex =="Female"]
+boxplot(females, males, main="Boxplot", names=c("Females", "Males"),xlab = "Height Distribution", ylab = "Height in (in.)", col = c("powderblue", "mistyrose"))
